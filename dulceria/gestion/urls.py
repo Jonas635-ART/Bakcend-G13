a0854b5f868e1c99d3f2d711f760b88ce5c1e264
unknown <jonasazula8901@gmail.com>
2023-08-28 12:16:31 -0500

    week06-final

--- dulceria/gestion/urls.py
+++ dulceria/gestion/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from .views import PagInicio, devolverHoraServidor, CategoriasController
+from .views import (PagInicio, 
+                    devolverHoraServidor, 
+                    CategoriasController, 
+                    CategoriaController, 
+                    Golosinascontroller,
+                    GolosinaController)
 
 urlpatterns = [
     path('inicio', PagInicio),
     path('status', devolverHoraServidor),
     path('categorias', CategoriasController.as_view()),
+    path('categoria/<id>', CategoriaController.as_view()),
+    path('golosinas', Golosinascontroller.as_view()),
+    path('golosina/<id>', GolosinaController.as_view())
 ]
 
 
